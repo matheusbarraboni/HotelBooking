c36284c93b069125162ef92348012b7a823285eb
Matheus Cabrini <matheusbarraboni@gmail.com>
2024-07-29 21:25:40 -0300

    feat: starting the Data project

--- BookingService/Consumers/API/Program.cs
+++ BookingService/Consumers/API/Program.cs
@@ -1,9 +1,19 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+#region DB wiring up
+var connectionString = builder.Configuration.GetConnectionString("Main");
+builder.Services.AddDbContext<HotelDbContext>(
+    options => options.UseSqlServer(connectionString));
+#endregion
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
